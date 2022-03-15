2f7ff835260a07b55ee9587bb2adddb106576d16
Burak Kaygusuz <kaygusuzburak@gmail.com>
2022-03-15 13:35:55 +0300

    Add android and iOS options

---
+++ src/test/java/io/github/burakkaygusuz/appiumspringboot/config/AndroidConfig.java
@@ -0,0 +1,51 @@
+package io.github.burakkaygusuz.appiumspringboot.config;
+
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.net.URL;
+
+@Configuration
+@PropertySource("classpath:device.properties")
+public class AndroidConfig {
+
+    @Value("${android.version}")
+    private String version;
+
+    @Value("${android.app.path}")
+    private URL app;
+
+    @Value("${android.device.name}")
+    private String deviceName;
+
+    @Value("${android.udid}")
+    private String udid;
+
+    @Value("${android.avd.name}")
+    private String avdName;
+
+    @Value("${app.activity}")
+    private String appActivity;
+
+    @Value("${app.package}")
+    private String appPackage;
+
+    @Bean
+    public UiAutomator2Options uiAutomator2Options() {
+        return new UiAutomator2Options()
+                .setPlatformVersion(version)
+                .setApp(app)
+                .setDeviceName(deviceName)
+                .setUdid(udid)
+                .setAvd(avdName)
+                .setAppActivity(appActivity)
+                .setAppPackage(appPackage)
+                .setNoReset(false)
+                .skipUnlock()
+                .setAutoGrantPermissions(true)
+                .setIsHeadless(true);
+    }
+}
---
+++ src/test/java/io/github/burakkaygusuz/appiumspringboot/config/IOSConfig.java
@@ -0,0 +1,43 @@
+package io.github.burakkaygusuz.appiumspringboot.config;
+
+import io.appium.java_client.ios.options.XCUITestOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.net.URL;
+
+@Configuration
+@PropertySource("classpath:device.properties")
+public class IOSConfig {
+
+    @Value("${ios.version}")
+    private String version;
+
+    @Value("${ios.app.path}")
+    private URL app;
+
+    @Value("${ios.device.name}")
+    private String deviceName;
+
+    @Value("${ios.udid}")
+    private String udid;
+
+    @Value("${bundle.id}")
+    private String bundleId;
+
+    @Bean
+    public XCUITestOptions xcuiTestOptions() {
+        return new XCUITestOptions()
+                .setPlatformVersion(version)
+                .setApp(app)
+                .setDeviceName(deviceName)
+                .setUdid(udid)
+                .setBundleId(bundleId)
+                .setNoReset(false)
+                .setAbsoluteWebLocations(true)
+                .setAutoAcceptAlerts(true)
+                .setIsHeadless(true);
+    }
+}
